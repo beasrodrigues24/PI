91138230086f5059e42a7ea4d5e93f206b51f6fa
beatrizsilvarodrigues <beatrizsilvarodrigues12@gmail.com>
2020-08-30 23:53:44 +0100

    Melhoria

--- 100Questoes/100Q.c
+++ 100Questoes/100Q.c
@@ -391,20 +391,13 @@ int limpaEspacos (char t[]) {
     return i;
 }
 
-// Q26 -- stolen
-void insere (int v[], int N, int x) {
-    for(int i = 0; i < N; i++) {
-        if(v[i] > x) {
-            for(int j = N; j > i; j--) {
-                v[j] = v[j - 1];
-            }
-            v[i] = x;
-            break;
-        }
-        if(i == N - 1) {
-            v[N] = x;
-        }
-    }
+// Q26 
+void insere(int v[], int N, int x) {
+    int i;
+    for (i = 0; i < N && v[i] < x; i++);
+    for (; N > i; N--)
+        v[N] = v[N-1];
+    v[i] = x;
 }
 
 // Q27
